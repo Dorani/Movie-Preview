82ab3408b18940fcfa4ab95fe1c8bf8a9e7e89d8
Seif Dorani <seif.dorani@gmail.com>
2016-11-03 15:14:26 -0400

    create index.js, pulling in module and assigning variables for starting app

---
+++ server/index.js
@@ -0,0 +1,5 @@
+var express = require('express'); // pulling in that module, assigning it this variable
+var mangoose = require('mongoose');
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override');
+var _ = require('lodash');
