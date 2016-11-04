a0d4185ad9bbf97ee55be004ddb88aa584bf9ffe
Seif <seif.dorani@gmail.com>
2016-11-03 22:28:38 -0400

    fixed typos in index.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 var express = require('express'); // pulling in that module, assigning it this variable
-var mangoose = require('mongoose');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override');
 var _ = require('lodash');
 
- var app = express();
+var app = express();
 
- //Add Middleware for REST API's
- app.use(bodyParser.urlencoded({extended: true}));
- app.use(bodyParser.json());
- app.use(methodOverride('X-HTTP-Method-Override'));
+//Add Middleware for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
 
 //Connect to MongoDB
-mongoose.connect('mongodb://localhost/Movie-Preview');
-mongoose.connection.once('open,' function() {
+mongoose.connect('mongodb://localhost/meanapp');
+mongoose.connection.once('open', function() {
   console.log('Listening on port 3000...');
   app.listen(3000);
 });
